b4c635d8fe5a9c1a121b264b4d33b8a3655ec440
Vidyashree-DC <89985657+Vidyashree-DC@users.noreply.github.com>
2021-09-03 19:05:52 +0530

    initial commit

---
+++ Aditionofintegerndoublevalues.java
@@ -0,0 +1,12 @@
+class Aditionofintegerndoublevalues{
+public static void main(String[] args){
+	int a;
+	double b;
+	double c;
+	a=80;
+	b=50.65;
+	c=a+b;
+	
+System.out.println("Adition of 2 integer n double values are:"+c);
+}
+}
